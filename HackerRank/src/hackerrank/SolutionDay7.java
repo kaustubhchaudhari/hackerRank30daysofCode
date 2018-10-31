ec0a03fb4a2c305b5b08718c1c9af230f22e1da0
Kaustubh Chaudhari <chaudhari.k@husky.neu.edu>
2018-10-31 19:15:48 -0400

    Solution day7,8

---
+++ HackerRank/src/hackerrank/SolutionDay7.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author chaudhari.k
+ */
+public class SolutionDay7 {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = Integer.parseInt(arrItems[arrItems.length - i -1]);
+            System.out.print(arr[i]);
+            System.out.print(" ");
+        }
+
+        scanner.close();
+    }
+}
---
+++ HackerRank/src/hackerrank/SolutionDay8.java
@@ -0,0 +1,44 @@
+ /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ *
+ * @author chaudhari.k
+ */
+public class SolutionDay8 {
+    public static void main(String [] args){
+        
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        Map<String, Integer> phoneBook = new HashMap<String, Integer>();
+
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            // Write code here
+            phoneBook.put(name, phone);
+            
+        }
+
+        while(in.hasNext()){
+            String s = in.next();
+            // Write code here
+            if(phoneBook.containsKey(s)){
+                System.out.println(s + "=" + phoneBook.get(s));
+            }
+            else{
+                System.out.println("Not found");
+            }
+        }
+        in.close();
+
+    }
+}
